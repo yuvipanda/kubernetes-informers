f934d33c6116a094120c317348bd04f52802a1e0
yuvipanda <yuvipanda@gmail.com>
2018-12-02 10:03:12 -0800

    Read python requirements from requirements.txt
    Add a dev-requirements.txt too

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from os.path import exists
 from setuptools import setup, find_packages
 
+install_requires=[
+    l for l in open('requirements.txt').read().strip().split('\n')
+    if not l.startswith('#')
+]
+
+
 setup(
     name='kubernetes-informers',
     version='0.1.0-dev',
@@ -13,4 +19,5 @@ setup(
     packages=find_packages(),
     long_description=(open('README.rst').read() if exists('README.rst') else ''),
     zip_safe=False,
+    install_requires=install_requires
 )
